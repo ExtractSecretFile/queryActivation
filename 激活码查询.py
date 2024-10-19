ab84f2b2833852d23a3c6250c3744bca5caa1699
poly000 <1348292515a@gmail.com>
2024-10-19 22:15:12 +0800

    fix: activation single query

--- 激活码查询.py
+++ 激活码查询.py
@@ -1,4 +1,5 @@
 from query import query_by_activation_code
 
 activation_code = input("请输入激活码: ")
-query_by_activation_code(activation_code)
+result = query_by_activation_code(activation_code)
+print(activation_code, result)
