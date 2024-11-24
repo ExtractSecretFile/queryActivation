df9f8fc9765a72edd39716828216859b88ac780c
poly000 <1348292515a@gmail.com>
2024-11-24 19:46:28 +0800

    enhance: support windows

--- query-rev-enc.py
+++ query-rev-enc.py
@@ -4,9 +4,10 @@ from querylib import query_by_encrypted_sn
 
 machine_code = input("请输入注册码: ")
 enc_sn = (
-    subprocess.check_output(["sh", "-c", f"echo {machine_code} | ./code_to_key"])
+    subprocess.check_output(["./code_to_key"], input=machine_code.encode())
     .decode("utf-8")
     .strip()
 )
 
+print("将查询授权码:", enc_sn)
 query_by_encrypted_sn(enc_sn)
